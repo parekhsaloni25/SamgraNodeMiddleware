1b5a50fda98dd394ddc16f33fcc05a99d83e0e41
urvish shah <urvish_s@techjoomla.com>
2022-02-01 15:27:13 +0530

    changed response structure for groupMembership apis

--- src/groupMembership/groupMembership.service.ts
+++ src/groupMembership/groupMembership.service.ts
@@ -1,5 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { response } from 'express';
+import { ErrorResponse } from './../error-response';
+import { SuccessResponse } from './../success-response';
 import { getManager, Repository } from 'typeorm';
 import { GroupMembershipDto } from './dto/groupMembership.dto';
 import { GroupMembership } from './groupMembership.entity';
@@ -16,33 +19,129 @@ export class GroupMembershipService {
     private readonly groupMembershipRepository: Repository<GroupMembership>
   ) {}
   
-  public async createGroupMembership(groupMembershipDto: GroupMembershipDto): Promise<GroupMembership>  {
-    return await this.groupMembershipRepository.save(groupMembershipDto);
+  public async createGroupMembership(groupMembershipDto: GroupMembershipDto): Promise<SuccessResponse>  {
+    try{
+      const data = await this.groupMembershipRepository.save(groupMembershipDto);
+      
+      return new SuccessResponse({
+        statusCode : response.statusCode,
+        message :'GroupMembership is created Successfully',
+        data : data,
+      });
+    } catch(e) {
+      var error = new ErrorResponse({
+        errorCode : e.response.status,
+        errorMessage : e.response.data.params.errmsg
+      })
+      throw new HttpException(error, e.response.status);
+    }
   }
 
-  public async getGroupMembership(): Promise<GroupMembership[]>  {
-    return await this.groupMembershipRepository.find();
+  public async getGroupMembership(): Promise<SuccessResponse>  {
+    try{
+      const data = await this.groupMembershipRepository.find();
+      
+      return new SuccessResponse({
+        statusCode : response.statusCode,
+        message :'GroupMemberships found Successfully',
+        data : data,
+      });
+    } catch(e) {
+      var error = new ErrorResponse({
+        errorCode : e.response.status,
+        errorMessage : e.response.data.params.errmsg
+      })
+      throw new HttpException(error, e.response.status);
+    }
   }
 
-  public async findGroupMembershipById(groupMembershipId:string): Promise<GroupMembership>  {
-    return await this.groupMembershipRepository.findOne(groupMembershipId);
+  public async findGroupMembershipById(groupMembershipId:string): Promise<SuccessResponse>  {
+    try{
+      const data = await this.groupMembershipRepository.findOne(groupMembershipId);
+      
+      return new SuccessResponse({
+        statusCode : response.statusCode,
+        message :'GroupMembership found Successfully by its id',
+        data : data,
+      });
+    } catch(e) {
+      var error = new ErrorResponse({
+        errorCode : e.response.status,
+        errorMessage : e.response.data.params.errmsg
+      })
+      throw new HttpException(error, e.response.status);
+    }
   }
 
-  public async findGroupMembershipBySchoolId(schoolId:string): Promise<GroupMembership[]>  {
-    return await this.groupMembershipRepository.find({schoolId});
+  public async findGroupMembershipBySchoolId(schoolId:string): Promise<SuccessResponse>  {
+    try{
+      const data = await this.groupMembershipRepository.find({schoolId});
+      
+      return new SuccessResponse({
+        statusCode : response.statusCode,
+        message :'GroupMembership found Successfully by schoolId',
+        data : data,
+      });
+    } catch(e) {
+      var error = new ErrorResponse({
+        errorCode : e.response.status,
+        errorMessage : e.response.data.params.errmsg
+      })
+      throw new HttpException(error, e.response.status);
+    }
   }
 
-  public async findGroupMembershipByUserId(userId:string): Promise<GroupMembership[]>  {
-    return await this.groupMembershipRepository.find({userId});
+  public async findGroupMembershipByUserId(userId:string): Promise<SuccessResponse>  {
+    try{
+      const data = await this.groupMembershipRepository.find({userId});
+      
+      return new SuccessResponse({
+        statusCode : response.statusCode,
+        message :'GroupMembership found Successfully by userId',
+        data : data,
+      });
+    } catch(e) {
+      var error = new ErrorResponse({
+        errorCode : e.response.status,
+        errorMessage : e.response.data.params.errmsg
+      })
+      throw new HttpException(error, e.response.status);
+    }
   }
 
-  public async updateGroupMembership(groupMembershipId:string, updateGroupMembershipDto:GroupMembershipDto): Promise<string>  {
-    const a = await this.groupMembershipRepository.update(groupMembershipId, updateGroupMembershipDto);
-    return 'groupMembership is updated successfully.';
+  public async updateGroupMembership(groupMembershipId:string, updateGroupMembershipDto:GroupMembershipDto): Promise<SuccessResponse>  {
+    try{
+      const data = await this.groupMembershipRepository.update(groupMembershipId, updateGroupMembershipDto);
+      
+      return new SuccessResponse({
+        statusCode : response.statusCode,
+        message :'GroupMembership is updated Successfully',
+        data : data,
+      });
+    } catch(e) {
+      var error = new ErrorResponse({
+        errorCode : e.response.status,
+        errorMessage : e.response.data.params.errmsg
+      })
+      throw new HttpException(error, e.response.status);
+    }
   }
 
-  public async deleteGroupMembership(groupMembershipId:string): Promise<string>  {
-    const grpDeleted = await this.groupMembershipRepository.delete(groupMembershipId);
-    return 'groupMembership is deleted successfully.';
+  public async deleteGroupMembership(groupMembershipId:string): Promise<SuccessResponse>  {
+    try{
+      const data = await this.groupMembershipRepository.delete(groupMembershipId);
+      
+      return new SuccessResponse({
+        statusCode : response.statusCode,
+        message :'GroupMembership is deleted Successfully',
+        data : data,
+      });
+    } catch(e) {
+      var error = new ErrorResponse({
+        errorCode : e.response.status,
+        errorMessage : e.response.data.params.errmsg
+      })
+      throw new HttpException(error, e.response.status);
+    }
   }
 }
